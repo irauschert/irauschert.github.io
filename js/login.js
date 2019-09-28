f39f033fcad5e61705dd942c8e1c95df3169dc97
irauschert <54558637+irauschert@users.noreply.github.com>
2019-09-28 17:31:55 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -16,19 +16,7 @@ function login() {
 }
 
 function onSignIn() {
-    if (auth2.isSignedIn.get()) {
-        var profile = auth2.currentUser.get().getBasicProfile();
-        console.log('ID: ' + profile.getId());
-        console.log('Full Name: ' + profile.getName());
-        console.log('Given Name: ' + profile.getGivenName());
-        console.log('Family Name: ' + profile.getFamilyName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail());
-
-        localStorage.setItem("username", profile.getName());
-
-        alert(console.log('ID: ' + profile.getId()))
-      }
+    alert("has entrado correctamente con Google, bien yo!")
     window.location = "http://irauschert.github.io/portada.html"
 
 }
