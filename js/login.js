6236e1c0e1a2831107e5fe4172e779588c1c5d82
irauschert <54558637+irauschert@users.noreply.github.com>
2019-09-28 17:44:45 -0300

    544

--- js/login.js
+++ js/login.js
@@ -16,22 +16,7 @@ function login() {
 }
 
 function onSignIn() {
-    alert("has entrado correctamente con Google, bien yo!");
-    
-    var auth2 = gapi.auth2.init();
-    var profile = auth2.currentUser.get().getBasicProfile();
-    console.log('ID: ' + profile.getId());
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
-
-    usernameToShow = profile.getName()
-    localStorage.setItem("username", usernameToShow);
-
-    alert(usernameToShow);
-
+    alert("Has entrado correctamente con Google, bien yo!");
     window.location = "http://irauschert.github.io/portada.html"
 
 }
