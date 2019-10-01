a3fe47cb7ebd8cbebfe52f6d63fc492bf6c9390e
irauschert <54558637+irauschert@users.noreply.github.com>
2019-10-01 15:53:00 -0300

    352

--- js/portada.js
+++ js/portada.js
@@ -7,11 +7,4 @@ function logout(){
     alert("Estás a punto de abandonar el sitio, ¿Estás seguro?")
     window.location.replace("index.html");
 }
-
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-    });
-  }
-
   showUsername();
