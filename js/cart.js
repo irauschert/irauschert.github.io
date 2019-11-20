778295ff1676d5c68f6de55a75f1ca9710efd9a3
Inés Rauschert <ines.rauschert@gmail.com>
2019-11-20 19:54:05 -0300

    2011

--- js/cart.js
+++ js/cart.js
@@ -6,7 +6,10 @@ let shippingCostUYU = 0;
 let shippingCostUSD = 0;
 let shippingPercentage = 0.15;
 let total = 0;
-currentArticlesArray = [];
+let currentArticlesArray = [];
+let productUnitCostUYUArray = [];
+let productUnitCostUSDArray = [];
+let productUnitCountArray = [];
 
 function diasCorridos(diasHabiles) {
     var businessDays = diasHabiles
@@ -47,48 +50,12 @@ function hidePaymentTypeNotSelected() {
     }
 }
 
-function validateForms() {
-    var forms = document.getElementsByClassName('needs-validation'); //Genera un elemento que no es un ARRAY, pero sobre el que se puede iterar.
-    var validation = Array.prototype.filter.call(forms, function (form) {
-        form.addEventListener('submit', function (event) {
-            if (form.checkValidity() === false) {
-                event.preventDefault();
-                event.stopPropagation();
-            }
-            form.classList.add('was-validated');
-        }, false);
-    });
-};
-
-function validateDataFromForms() {
-    var adress = document.getElementById("shippingAdressForm").elements[0];
-    if (adress.value == "") {
-        return false;
-    }
-
-    var number = document.getElementById("shippingAdressForm").elements[1];
-    alert(number.value);
-    if (number.value > 10000 || number.value == 0) {
-        adress.setCustomValidity("Campo Invalido");
-    } else {
-        adress.setCustomValidity(""); // Lo dejo vacio para indicar que esta Ok
-    }
-
-    var corner = document.getElementById("shippingAdressForm").elements[2];
-    if (corner.value == "") {
-        corner.setCustomValidity("Campo Invalido");
-    } else {
-        adress.setCustomValidity(""); // Lo dejo vacio para indicar que esta Ok
-    }
-    validateForms();
-}
-
 function showArticles(array) {
     let articlesHTMLtoAppend = '<div class="row">';
     for (let i = 0; i < array.length; i++) {
         let article = array[i];
         articlesHTMLtoAppend += `
-            <div class="col-sm-4 col-md-4 col-lg-5 col-xl-2">
+            <div class="col-6 col-sm-4 col-md-4 col-lg-3 col-xl-2">
                 <div class="card shadow-sm">
                     <img src=" ${article.src}" class="p-2 card-img-top">
                     <div class="card-body">
@@ -96,9 +63,9 @@ function showArticles(array) {
                         <small class="text-muted"> Precio unitario: ${article.currency} ${article.unitCost} </small>
                         <form>
                             <label for="articleCount">Cantidad</label>
-                            <input id="articleCount" class="form-control form-control-sm articleCount" type="number" min="0" value="` + article.count + `">
+                            <input id="articleCount" class="form-control" type="number" min="0" value="` + article.count + `">
+                            <button type="submit" disabled style="display: none" aria-hidden="true"></button>
                         </form>
-                        <p class="text-right">Subtotal del Item: ${article.currency} ${article.unitCost * article.count} </p>
                     </div>
                 </div>
             </div>
@@ -180,18 +147,9 @@ document.addEventListener("DOMContentLoaded", function (e) {
             currentArticlesArray.push({ name: "Piscina para niños", count: 1, unitCost: 790, currency: "UYU", src: "img/pool1.jpg" })
             currentArticlesArray.push({ name: "Inflable con Tobogan ", count: 1, unitCost: 2390, currency: "UYU", src: "img/inflable1.jpg" })
             showArticles(currentArticlesArray);
+            showSubtotal(currentArticlesArray);
             updateShippingCost(0.15);
         }
-
-        var arrayByClass = document.getElementsByClassName("articleCount");
-
-        for (var i = 0; i < arrayByClass.length; i++) {
-            arrayByClass[i].addEventListener("change", function () {
-                document.getElementById('goldradio').checked = true;
-                updateSubtotal(currentArticlesArray);
-                updateShippingCost(0.15);
-            })
-        };
     });
 
     document.getElementById("goldradio").addEventListener("change", function () {
@@ -218,4 +176,143 @@ document.addEventListener("DOMContentLoaded", function (e) {
         document.getElementById("transferenciaBancaria").style.display = "none";
         document.getElementById("tarjetaDeCredito").style.display = "";
     });
+
+    let formularioDireccionHTML = document.getElementById("adressForm");
+    document.getElementById("errorInputStreet").style.display = 'none';
+    document.getElementById("errorInputStreetNumber").style.display = 'none';
+    document.getElementById("errorInputCorner").style.display = 'none';
+    document.getElementById("errorInputCP").style.display = 'none';
+    document.getElementById("errorInputCountry").style.display = 'none';
+    document.getElementById("errorInputState").style.display = 'none';
+
+    formularioDireccionHTML.addEventListener('submit', function (evento) {
+        evento.preventDefault();
+
+        let inputStreetNameHTML = formularioDireccionHTML.inputStreetName;
+        let errorInputStreetHTML = document.getElementById('errorInputStreet');
+        if (inputStreetNameHTML.value.length < 1) {
+            errorInputStreetHTML.style.display = 'block'
+            errorInputStreetHTML.innerHTML = `<small class="ml-3"> Debe ingresar el nombre de una calle</small>`
+            inputStreetNameHTML.classList.add('validated')
+            inputStreetNameHTML.classList.replace('validated', 'error')
+            inputStreetNameHTML.validity.valid = 'false';
+        } else {
+            errorInputStreetHTML.style.display = 'none'
+            inputStreetNameHTML.classList.add('error')
+            inputStreetNameHTML.classList.replace('error', 'validated')
+            inputStreetNameHTML.validity.valid = 'true';
+        }
+
+        let inputStreetNumberHTML = formularioDireccionHTML.inputStreetNumber;
+        let errorInputStreetNumberHTML = document.getElementById('errorInputStreetNumber');
+        if (inputStreetNumberHTML.value == 0 || inputStreetNumberHTML.value > 10000) {
+            errorInputStreetNumberHTML.style.display = 'block'
+            errorInputStreetNumberHTML.innerHTML = `<small class="ml-3"> Debe ingresar un numero de puerta de hasta 4 digitos </small>`
+            inputStreetNumberHTML.classList.add('validated')
+            inputStreetNumberHTML.classList.replace('validated', 'error')
+        } else {
+            errorInputStreetNumberHTML.style.display = 'none'
+            inputStreetNumberHTML.classList.add('error')
+            inputStreetNumberHTML.classList.replace('error', 'validated')
+        }
+
+        let inputCornerNameHTML = formularioDireccionHTML.inputCornerName;
+        let errorInputCornerHTML = document.getElementById('errorInputCorner');
+        if (inputCornerNameHTML.value.length < 1) {
+            errorInputCornerHTML.style.display = 'block'
+            errorInputCornerHTML.innerHTML = `<small class="ml-3"> Debe ingresar el nombre de una calle</small>`
+            inputCornerNameHTML.classList.add('validated')
+            inputCornerNameHTML.classList.replace('validated', 'error')
+        } else {
+            errorInputCornerHTML.style.display = 'none'
+            inputCornerNameHTML.classList.add('error')
+            inputCornerNameHTML.classList.replace('error', 'validated')
+        }
+
+        let inputCPHTML = formularioDireccionHTML.inputZIPCode;
+        let errorInputCP = document.getElementById('errorInputCP');
+        if (inputCPHTML.value < 1 || inputCPHTML.value > 100000) {
+            errorInputCP.style.display = 'block'
+            errorInputCP.innerHTML = `<small class="ml-3"> Debe ingresar un numero de 5 cifras</small>`
+            inputCPHTML.classList.add('validated')
+            inputCPHTML.classList.replace('validated', 'error')
+        } else {
+            errorInputCP.style.display = 'none'
+            inputCPHTML.classList.add('error')
+            inputCPHTML.classList.replace('error', 'validated')
+        }
+
+        let inputCountryHTML = formularioDireccionHTML.country;
+        let errorInputCountry = document.getElementById('errorInputCountry');
+        if (inputCountryHTML.value == -1) {
+            errorInputCountry.style.display = 'block'
+            errorInputCountry.innerHTML = `<small class="ml-3"> Debe seleccionar un Pais</small>`
+            inputCountryHTML.classList.add('validated')
+            inputCountryHTML.classList.replace('validated', 'error')
+        } else {
+            errorInputCountry.style.display = 'none'
+            inputCountryHTML.classList.add('error')
+            inputCountryHTML.classList.replace('error', 'validated')
+        }
+
+        let inputStateHTML = formularioDireccionHTML.state;
+        let errorInputState = document.getElementById('errorInputState');
+        if (inputStateHTML.value == 0) {
+            errorInputState.style.display = 'block'
+            errorInputState.innerHTML = `<small class="ml-3"> Debe seleccionar un Departamento </small>`
+            inputStateHTML.classList.add('validated')
+            inputStateHTML.classList.replace('validated', 'error')
+        } else {
+            errorInputState.style.display = 'none'
+            inputStateHTML.classList.add('error')
+            inputStateHTML.classList.replace('error', 'validated')
+        }
+
+        if (inputStreetNameHTML.validity.valid == true) {
+            AdressHTML = `Recibira su compra en ${inputStreetNameHTML.value} ${inputStreetNumberHTML.value}, Codigo Postal ${inputCPHTML.value}.`;
+            document.getElementById('adressToShow').innerHTML = AdressHTML;
+        }
+    })
+
+
+    let formularioPagoHTML = document.getElementById("paymentTypeForm");
+    document.getElementById("errorCardNumber").style.display = 'none';
+    document.getElementById("errorCardCVV").style.display = 'none';
+    document.getElementById("errorCardExpDate").style.display = 'none';
+    document.getElementById("errorBankAccountNumber").style.display = 'none';
+
+    formularioPagoHTML.addEventListener('submit', function (evento) {
+        evento.preventDefault();
+
+        let inputCreditCardNumberHTML = formularioPagoHTML.creditCardNumber;
+        let errorCreditCardNumberHTML = document.getElementById('errorCardNumber');
+        if (inputCreditCardNumberHTML.value > 10000000000000000 || inputCreditCardNumberHTML.value < 100000000000000) {
+            errorCreditCardNumberHTML.style.display = 'block'
+            errorCreditCardNumberHTML.innerHTML = `<small class="ml-3"> El numero ingresado no es valido</small>`
+            inputCreditCardNumberHTML.classList.add('validated')
+            inputCreditCardNumberHTML.classList.replace('validated', 'error')
+            inputCreditCardNumberHTML.validity.valid = 'false';
+        } else {
+            errorCreditCardNumberHTML.style.display = 'none'
+            inputCreditCardNumberHTML.classList.add('error')
+            inputCreditCardNumberHTML.classList.replace('error', 'validated')
+            inputCreditCardNumberHTML.validity.valid = 'true';
+        }
+
+        let inputCreditCardCVVHTML = formularioPagoHTML.creditCardCVV;
+        let errorCreditCardCVVHTML = document.getElementById('errorCardCVV');
+        if (inputCreditCardCVVHTML.value > 999 || inputCreditCardNumberHTML.value < 100) {
+            errorCreditCardCVVHTML.style.display = 'block'
+            errorCreditCardCVVHTML.innerHTML = `<small class="ml-3"> El numero ingresado no es valido</small>`
+            inputCreditCardCVVHTML.classList.add('validated')
+            inputCreditCardCVVHTML.classList.replace('validated', 'error')
+            inputCreditCardCVVHTML.validity.valid = 'false';
+        } else {
+            errorCreditCardCVVHTML.style.display = 'none'
+            inputCreditCardCVVHTML.classList.add('error')
+            inputCreditCardCVVHTML.classList.replace('error', 'validated')
+            inputCreditCardCVVHTML.validity.valid = 'true';
+        }
+    })
+
 });
