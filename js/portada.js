523b0e33310715ee028893a639916058cbf87a05
Inés Rauschert <ines.rauschert@gmail.com>
2019-11-29 17:01:53 -0300

    501

--- js/portada.js
+++ js/portada.js
@@ -5,10 +5,6 @@ function showUsername(){
 
 function signOut() {
     alert("Estás a punto de abandonar el sitio, ¿Estás seguro?")
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-    });
     window.location.replace("index.html");
 }
 
@@ -17,4 +13,3 @@ function onLoad() {
         gapi.auth2.init();
     });
 }
-showUsername();
